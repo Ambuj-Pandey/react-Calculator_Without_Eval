ca3ec6c5d5e0828acf91a6d1d0f834f0bd886f10
Ambuj-Pandey <78492933+Ambuj-Pandey@users.noreply.github.com>
2023-09-03 07:43:40 +0000

    Added - Adding digit and Symbols functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
+import { useReducer } from 'react';
+import Digit from './Digit';
+import OperationButton from './OperationButton';
 import './App.css';
 
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+}
+const reducer = (state, action) =>{
+
+}
+
 function App() {
+const[{ curr, prev, oper}, dispatch ] = useReducer(reducer, {});
+
   return (
     <div className='Calculator-grid'>
       <div className='Ouput'>
-        <div className='PrevOP'>1234*</div>
-        <div className='CurOP'>123</div>
+        <div className='PrevOP'>{prev} {oper}</div>
+        <div className='CurOP'>{curr}</div>
       </div>
       <button className='span-two'>AC</button>
       <button>DEL</button>
-      <button>÷</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
+      <OperationButton operation="÷" dispatch={dispatch} />
+      <Digit digit="1" dispatch={dispatch} />
+      <Digit digit="2" dispatch={dispatch} />
+      <Digit digit="3" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
+      <Digit digit="4" dispatch={dispatch} />
+      <Digit digit="5" dispatch={dispatch} />
+      <Digit digit="6" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
+      <Digit digit="7" dispatch={dispatch} />
+      <Digit digit="8" dispatch={dispatch} />
+      <Digit digit="9" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
+      <Digit digit="." dispatch={dispatch} />
+      <Digit digit="0" dispatch={dispatch} />
       <button className='span-two'>=</button>
     </div>
   );
